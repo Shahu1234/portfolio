10c5157d7a27b898e310f5902ff2e6efbc6af806
Shahu1234 <shahajadi135@gmail.com>
2025-08-10 13:49:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ var tablinks = document.getElementsByClassName("tab-links");
            event.currentTarget.classList.add("active-link");
            document.getElementById(tabname).classList.add("active-tab");
         }
-        
+
+function submitted() {
+    var name = document.querySelector('input[name="Name"]').value;
+    var email = document.querySelector('input[name="email"]').value;
+
+
+    if (name === "" || email === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    alert("Form submitted successfully!");
+}
